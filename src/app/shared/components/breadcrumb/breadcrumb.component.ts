6a593da84d7f8c8d789d74269b7f6e90639c438e
MohammedmahMoudabdelkarim <mohammedmahmoudabdelkarim.gmail.com>
2025-01-25 14:32:07 +0200

    uninstall ngx-breadcrumb package not support in AngV19

--- src/app/shared/components/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -6,29 +6,34 @@ import {
   RouterModule,
 } from '@angular/router';
 import { BreadcrumbModule } from 'primeng/breadcrumb';
-import { NgxSignalBreadcrumbService } from 'ngx-signal-breadcrumb';
 import { MenuItem } from 'primeng/api';
 import { CommonModule } from '@angular/common';
-import { filter } from 'rxjs';
-import { BreadCrumb } from '../../../core/models/breadcrumb.model';
+import { filter, map } from 'rxjs';
 @Component({
   selector: 'app-breadcrumb',
   imports: [BreadcrumbModule, RouterModule, CommonModule],
   templateUrl: './breadcrumb.component.html',
 })
 export class BreadcrumbComponent {
-  readonly #ngxSignalBreadcrumbService: NgxSignalBreadcrumbService<BreadCrumb> =
-    inject(NgxSignalBreadcrumbService);
   readonly activatedRoute: ActivatedRoute = inject(ActivatedRoute);
   readonly router: Router = inject(Router);
-  items: MenuItem[] | any = this.#ngxSignalBreadcrumbService.breadcrumbs();
+  items: MenuItem[] | any = [];
   home: MenuItem | undefined;
-
   ngOnInit() {
+    this.items =
+      this.activatedRoute.snapshot.firstChild?.firstChild?.data['breadcrumbs'];
     this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe(() => {
-        this.items = this.#ngxSignalBreadcrumbService.breadcrumbs();
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(
+          () =>
+            this.activatedRoute.snapshot.firstChild?.firstChild?.data[
+              'breadcrumbs'
+            ]
+        )
+      )
+      .subscribe((breadcrumb) => {
+        this.items = breadcrumb;
       });
   }
 }
